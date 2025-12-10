7372387cd37f40aee5cc5769152446ff0d369580
ltakoushi18 <ltakoushi18@gmail.com>
2025-12-10 15:47:55 +1000

    updated drop down menu to display selected timezone

--- javascript/index.js
+++ javascript/index.js
@@ -16,6 +16,24 @@ function updateTime() {
   parisDateElement.innerHTML = parisTime.format("MMMM Do YYYY");
   parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
-
+function updateCity(event) {
+  let selectedCityTimeZone = event.target.value;
+  let cityName = selectedCityTimeZone.replace("_", " ").split("/")[1];
+  let selectedCityTime = moment().tz(selectedCityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `<div class="city">
+        <div>
+        <h2>${cityName}</h2>
+      <div class="date">${selectedCityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${selectedCityTime.format(
+        "h:mm:ss"
+      )}<small>${selectedCityTime.format(" A")}</small></div>
+      </div>
+        `;
+}
 updateTime();
 setInterval(updateTime, 1000);
+
+let selectedCity = document.querySelector("#selected-city");
+selectedCity.addEventListener("change", updateCity);
